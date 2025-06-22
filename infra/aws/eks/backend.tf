5e33a086c5e7aeb6e687910228068a13ea2000eb
Paul Carlton <paul.carlton414@gmail.com>
2025-06-22 13:05:02 +0100

    Remove use_lockfile from backend.tf files for Terraform compatibility

--- infra/aws/eks/backend.tf
+++ infra/aws/eks/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "dae-voters-tfstate"
     key            = "eks/terraform.tfstate"
     region         = "eu-west-1"
-    use_lockfile   = true
   }
 } 
--- infra/aws/vpc/backend.tf
+++ infra/aws/vpc/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "dae-voters-tfstate"
     key            = "vpc/terraform.tfstate"
     region         = "eu-west-1"
-    use_lockfile   = true
   }
 } 
